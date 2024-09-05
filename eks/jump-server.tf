eebe311fef5fdcd4a0549243bac8e744939dee8f
Madhukar Reddy <madhu@madhukarreddy.com>
2024-09-05 12:56:25 +0530

    update jump server key

--- eks/jump-server.tf
+++ eks/jump-server.tf
@@ -1,37 +1,37 @@
-resource "aws_instance" "jump" {
-    ami = "ami-053b0d53c279acc90"
-    disable_api_termination = false
-    instance_type = "t2.medium"
-    private_ip = "10.16.0.113"
-    key_name = "madhukey"
-    disable_api_stop = false
-    user_data = file("jump.sh")
-    root_block_device {
-        delete_on_termination = true
-        volume_size = 12
-        tags = {
-            Name = "Jump-Server-EKS"
-            PrivateIP = "10.16.0.113"
-        }
-    }
-    # subnet_id = aws_subnet.public.id
-    tags = {
-        Name = "Jump-server-EKS"
-        Prod = "True"
-        PrivateIP = "10.16.0.113"
-    }
-    tags_all = {
-        Name = "Jump-server-EKS"
-        Prod = "True"
-        PrivateIP = "10.16.0.113"
-    }
-    vpc_security_group_ids = [
-        aws_security_group.jump-sg.id
-    ]
-}
+# resource "aws_instance" "jump" {
+#     ami = "ami-053b0d53c279acc90"
+#     disable_api_termination = false
+#     instance_type = "t2.medium"
+#     private_ip = "10.16.0.113"
+#     key_name = "madhukey"
+#     disable_api_stop = false
+#     user_data = file("jump.sh")
+#     root_block_device {
+#         delete_on_termination = true
+#         volume_size = 12
+#         tags = {
+#             Name = "Jump-Server-EKS"
+#             PrivateIP = "10.16.0.113"
+#         }
+#     }
+#     subnet_id = aws_subnet.public.id
+#     tags = {
+#         Name = "Jump-server-EKS"
+#         Prod = "True"
+#         PrivateIP = "10.16.0.113"
+#     }
+#     tags_all = {
+#         Name = "Jump-server-EKS"
+#         Prod = "True"
+#         PrivateIP = "10.16.0.113"
+#     }
+#     vpc_security_group_ids = [
+#         aws_security_group.jump-sg.id
+#     ]
+# }
 
 
-output "jump-pip" {
-    value =aws_instance.jump.public_ip
+# output "jump-pip" {
+#     value =aws_instance.jump.public_ip
   
-}
+# }
--- eks/jump-sg.tf
+++ eks/jump-sg.tf
@@ -1,109 +1,109 @@
-resource "aws_security_group" "jump-sg" {
-    description = "JumpSG"
-    vpc_id = module.eks.aws_vpc.vpc.id
-    name = "jump-sg"
-    tags = {
-        Name = "Jump-SG"
-        CreatedBy = "Terraform"
-    }
-    tags_all = {
-        Name = "Jump-SG"
-        CreatedBy = "Terraform"
-    }
-    egress = [
-        {
-        cidr_blocks = [
-            "0.0.0.0/0"
-        ]
-        description = ""
-        from_port = 0
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "-1"
-        security_groups = []
-        self = false
-        to_port = 0
-        }
-    ]
-    ingress = [
-        {
-        cidr_blocks = [
-            "0.0.0.0/0"
-        ]
-        description = ""
-        from_port = 443
-        ipv6_cidr_blocks = ["::/0"]
-        prefix_list_ids = []
-        protocol = "tcp"
-        security_groups = []
-        self = false
-        to_port = 443
-        },
-        {
-        "cidr_blocks": [
-            "0.0.0.0/0"
-        ]
-        "description": "Allow traffic from public internet"
-        "from_port": 80
-        "ipv6_cidr_blocks": []
-        "prefix_list_ids": []
-        "protocol": "tcp"
-        "security_groups": []
-        "self": false
-        "to_port": 80
-        },
-         {
-        "cidr_blocks": [
-            "0.0.0.0/0"
-        ]
-        "description": "Allow traffic from public internet"
-        "from_port": 3306
-        "ipv6_cidr_blocks": []
-        "prefix_list_ids": []
-        "protocol": "tcp"
-        "security_groups": []
-        "self": false
-        "to_port": 3306
-        },
-        {
-        "cidr_blocks": [
-            "0.0.0.0/0"
-        ]
-        "description": "Allow SSH from public internet"
-        "from_port": 22
-        "ipv6_cidr_blocks": []
-        "prefix_list_ids": []
-        "protocol": "tcp"
-        "security_groups": []
-        "self": false
-        "to_port": 22
-        },
-        {
-        cidr_blocks = [
-            "0.0.0.0/0"
-        ]
-        description = ""
-        from_port = 0
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "-1"
-        security_groups = []
-        self = false
-        to_port = 0
-        }
-        #  {
-        # cidr_blocks = [
-        #     "0.0.0.0/0"
-        # ]
-        # description = ""
-        # from_port = 0
-        # ipv6_cidr_blocks = []
-        # prefix_list_ids = []
-        # protocol = "-1"
-        # security_groups = ["test-sg"]
-        # self = false
-        # to_port = 0
-        # }
+# resource "aws_security_group" "jump-sg" {
+#     description = "JumpSG"
+#     vpc_id = module.eks.aws_vpc.vpc.id
+#     name = "jump-sg"
+#     tags = {
+#         Name = "Jump-SG"
+#         CreatedBy = "Terraform"
+#     }
+#     tags_all = {
+#         Name = "Jump-SG"
+#         CreatedBy = "Terraform"
+#     }
+#     egress = [
+#         {
+#         cidr_blocks = [
+#             "0.0.0.0/0"
+#         ]
+#         description = ""
+#         from_port = 0
+#         ipv6_cidr_blocks = []
+#         prefix_list_ids = []
+#         protocol = "-1"
+#         security_groups = []
+#         self = false
+#         to_port = 0
+#         }
+#     ]
+#     ingress = [
+#         {
+#         cidr_blocks = [
+#             "0.0.0.0/0"
+#         ]
+#         description = ""
+#         from_port = 443
+#         ipv6_cidr_blocks = ["::/0"]
+#         prefix_list_ids = []
+#         protocol = "tcp"
+#         security_groups = []
+#         self = false
+#         to_port = 443
+#         },
+#         {
+#         "cidr_blocks": [
+#             "0.0.0.0/0"
+#         ]
+#         "description": "Allow traffic from public internet"
+#         "from_port": 80
+#         "ipv6_cidr_blocks": []
+#         "prefix_list_ids": []
+#         "protocol": "tcp"
+#         "security_groups": []
+#         "self": false
+#         "to_port": 80
+#         },
+#          {
+#         "cidr_blocks": [
+#             "0.0.0.0/0"
+#         ]
+#         "description": "Allow traffic from public internet"
+#         "from_port": 3306
+#         "ipv6_cidr_blocks": []
+#         "prefix_list_ids": []
+#         "protocol": "tcp"
+#         "security_groups": []
+#         "self": false
+#         "to_port": 3306
+#         },
+#         {
+#         "cidr_blocks": [
+#             "0.0.0.0/0"
+#         ]
+#         "description": "Allow SSH from public internet"
+#         "from_port": 22
+#         "ipv6_cidr_blocks": []
+#         "prefix_list_ids": []
+#         "protocol": "tcp"
+#         "security_groups": []
+#         "self": false
+#         "to_port": 22
+#         },
+#         {
+#         cidr_blocks = [
+#             "0.0.0.0/0"
+#         ]
+#         description = ""
+#         from_port = 0
+#         ipv6_cidr_blocks = []
+#         prefix_list_ids = []
+#         protocol = "-1"
+#         security_groups = []
+#         self = false
+#         to_port = 0
+#         }
+#         #  {
+#         # cidr_blocks = [
+#         #     "0.0.0.0/0"
+#         # ]
+#         # description = ""
+#         # from_port = 0
+#         # ipv6_cidr_blocks = []
+#         # prefix_list_ids = []
+#         # protocol = "-1"
+#         # security_groups = ["test-sg"]
+#         # self = false
+#         # to_port = 0
+#         # }
 
-    ]
-}
+#     ]
+# }
